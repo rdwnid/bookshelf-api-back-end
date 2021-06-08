283fef8b196ddba1b166ad984a923c78d3359578
Muhamad Ridwan <muhamadridwan@student.unsil.ac.id>
2021-06-09 01:42:53 +0700

    initial commit

---
+++ src/routes.js
@@ -0,0 +1,32 @@
+// eslint-disable-next-line max-len
+const {addBookHandler, getAllBookHandler, getBookByIdHandler, editBookByIdHandler, deletBookByIdHandler} = require('./handler');
+
+const routes = [
+  {
+    method: 'POST',
+    path: '/books',
+    handler: addBookHandler,
+  },
+  {
+    method: 'GET',
+    path: '/books',
+    handler: getAllBookHandler,
+  },
+  {
+    method: 'GET',
+    path: '/books/{bookId}',
+    handler: getBookByIdHandler,
+  },
+  {
+    method: 'PUT',
+    path: '/books/{bookId}',
+    handler: editBookByIdHandler,
+  },
+  {
+    method: 'DELETE',
+    path: '/books/{bookId}',
+    handler: deletBookByIdHandler,
+  },
+];
+
+module.exports = routes;
